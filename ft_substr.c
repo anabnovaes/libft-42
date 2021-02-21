4a6efbbf1f7150f84753dd40e236cd771aea8b9c
Ana Novaes <ana-paula1503@hotmail.com>
2021-02-21 14:47:41 -0300

    Incluindo ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 12:27:06 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/21 13:00:11 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/02/21 14:47:07 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char			*dest;
 	unsigned int	counter;
 	char			*org;
-	
+
 	counter = 0;
 	org = (char *)s;
-	if(len == 0 || !(dest = malloc(len + 1)))
-		return (NULL);
-	while(counter < (unsigned int)len)
+	dest = malloc(len + 1);
+	if (len == 0 || !dest)
+		return (dest);
+	while (counter < (unsigned int)len)
 	{
 		dest[counter] = org[counter + start];
 		counter++;
