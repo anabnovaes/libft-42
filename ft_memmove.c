2da37c466563ece7fe949bfff17cf8e9c42f1bad
Ana Novaes <ana.novaes@cloudwalk.io>
2021-02-21 11:50:33 -0300

    fix some memmove errors

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 19:21:40 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/21 11:48:43 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/02/21 11:50:15 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	void	*temp[ft_strlen((char *)src)];
 	
-	if(len == 0)
+	if(len == 0 || src == NULL)
 		return (NULL);
 	ft_memcpy(temp, src, len + 1);
 	ft_memcpy(dst, temp, len);
