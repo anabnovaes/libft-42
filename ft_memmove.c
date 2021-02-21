0063be347cf711541543518a6682cfdca5e55ab5
Ana Novaes <ana-paula1503@hotmail.com>
2021-02-21 12:24:50 -0300

    fix norminette errors in memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 19:21:40 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/21 11:53:55 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/02/21 12:20:01 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,22 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	void	*temp[ft_strlen((char *)src)];
-	
-	if (src == NULL)
-		return ((void *)src);
-	if(len == 0)
-		return (NULL);
-	ft_memcpy(temp, src, len + 1);
-	ft_memcpy(dst, temp, len);
+	unsigned char	*org;
+	unsigned char	*dest;
+
+	if ((src == NULL && dst == NULL) || len == 0)
+		return ((void *)dst);
+	if (src < dst)
+	{
+		org = (unsigned char *)src;
+		dest = (unsigned char *)dst;
+		while (len)
+		{
+			dest[len - 1] = org[len - 1];
+			len--;
+		}
+	}
+	else
+		ft_memcpy(dst, src, len);
 	return (dst);
 }
