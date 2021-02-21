5dc75474e97c23bdd1b5cfe2f9c460a5a1a35b76
Ana Novaes <ana-paula1503@hotmail.com>
2021-02-21 12:57:30 -0300

    fix substr errors

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 12:27:06 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/21 12:55:35 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/02/21 12:57:10 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	org = (char *)s;
 	if(len == 0 || !(dest = malloc(len + 1)))
 		return (NULL);
-	while(counter < (unsigned int)len + start)
+	while(counter < (unsigned int)len + start - 1)
 	{
 		dest[counter] = org[counter + start];
 		counter++;
 	}
-	
+	dest[counter] = '\0';
 	return (dest);
 }
