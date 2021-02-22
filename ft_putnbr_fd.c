2af519e535c2090f78a9ae841b2dd21df650e568
Ana Novaes <ana-paula1503@hotmail.com>
2021-02-22 20:44:45 -0300

    Add putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 17:52:09 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/22 19:22:37 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/02/22 20:41:33 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,24 +27,21 @@ static int		check_size(long int value)
 	return (size);
 }
 
-static void	cnvr_vlue(long int value, int size, int fd)
+static void		cnvr_vlue(long int value, int size, int fd)
 {
-	int		last_value;
-	int		counter;
-	char	string_print[size + 1];
-	int		sub_vlue;
-	int		i;
+	int			last_value;
+	char		string_print[size + 1];
+	int			counter;
+	int			i;
 
 	i = 0;
-	sub_vlue = 1;
 	counter = 1;
 	while (counter <= size)
 	{
 		last_value = value % 10;
 		value /= 10;
-		string_print[size - sub_vlue] = last_value + '0';
+		string_print[size - counter] = last_value + '0';
 		counter++;
-		sub_vlue++;
 	}
 	string_print[size] = '\0';
 	while (string_print[i] != '\0')
@@ -54,20 +51,17 @@ static void	cnvr_vlue(long int value, int size, int fd)
 	}
 }
 
-void	ft_putnbr_fd(int n, int fd)
+void			ft_putnbr_fd(int n, int fd)
 {
-	long int	lng_vlue;
+	long int	print_value;
 	int			size;
 
-	lng_vlue = n;
+	print_value = n;
 	if (n < 0)
 	{
-		write(1, "-", 1);
-		lng_vlue *= -1;
+		write(fd, "-", 1);
+		print_value *= -1;
 	}
-	size = check_size(lng_vlue);
-	cnvr_vlue(lng_vlue, size, fd);
+	size = check_size(print_value);
+	cnvr_vlue(print_value, size, fd);
 }
-
-
-
