702991748b85c2e1dfa25b13489ba46a1448705e
Ana Novaes <ana-paula1503@hotmail.com>
2021-02-27 16:26:16 -0300

    corrigindo strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 18:23:41 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/27 16:19:42 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/02/27 16:24:44 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char			*string;
 
 	counter = 0;
-	if (!(string = malloc(ft_strlen((char *)s + 1)* sizeof(char))))
+	if (!(string = malloc((ft_strlen((char *)s)+ 1)* sizeof(char))))
 		return (NULL);
 	if (s == NULL)
 		return (NULL);
