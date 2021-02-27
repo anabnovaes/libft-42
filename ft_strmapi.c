1d3ba36a0bf2c4983ddbcebacf8f679aca85f296
Ana Novaes <ana-paula1503@hotmail.com>
2021-02-27 16:19:53 -0300

    corrigindo alocacao em strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 18:23:41 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/27 16:16:08 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/02/27 16:19:42 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char			*string;
 
 	counter = 0;
-	if (!(string = calloc(ft_strlen((char *)s + 1), 1)))
+	if (!(string = malloc(ft_strlen((char *)s + 1)* sizeof(char))))
 		return (NULL);
 	if (s == NULL)
 		return (NULL);
@@ -27,5 +27,6 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 		string[counter] = f(counter, s[counter]);
 		counter++;
 	}
+	string[counter] = '\0';
 	return (string);
 }
