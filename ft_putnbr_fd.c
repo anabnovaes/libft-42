e4da4283e2e7c7ddbf80dd57513d873591d7fdb4
Ana Novaes <ana-paula1503@hotmail.com>
2021-02-27 16:14:34 -0300

    alterando putnbr para nao alocar memoria

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,22 +6,62 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 17:52:09 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/23 21:59:29 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/02/27 16:14:10 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		ft_putnbr_fd(int n, int fd)
+static int		check_size(long int value)
 {
-	char	*value;
-	size_t	counter;
+	int			size;
+	long int	compare_value;
 
-	value = ft_itoa(n);
-	counter = 0;
-	while (value[counter])
+	size = 1;
+	compare_value = value;
+	while ((compare_value / 10) > 0)
 	{
-		write(fd, &value[counter], 1);
+		compare_value /= 10;
+		size++;
+	}
+	return (size);
+}
+
+static void		cnvr_vlue(long int value, int size, int fd)
+{
+	int			last_value;
+	char		string_print[size + 1];
+	int			counter;
+	int			i;
+
+	i = 0;
+	counter = 1;
+	while (counter <= size)
+	{
+		last_value = value % 10;
+		value /= 10;
+		string_print[size - counter] = last_value + '0';
 		counter++;
 	}
+	string_print[size] = '\0';
+	while (string_print[i] != '\0')
+	{
+		write(fd, &string_print[i], 1);
+		i++;
+	}
+}
+
+void			ft_putnbr_fd(int n, int fd)
+{
+	long int	print_value;
+	int			size;
+
+	print_value = n;
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		print_value *= -1;
+	}
+	size = check_size(print_value);
+	cnvr_vlue(print_value, size, fd);
 }
