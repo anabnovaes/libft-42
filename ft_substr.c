3688a1bcbebff6c01968a5a9140f0cbb076b97d2
Ana Novaes <ana-paula1503@hotmail.com>
2021-02-28 10:54:20 -0300

    corrigindo valor nulo na substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 12:27:06 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/28 10:47:27 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/02/28 10:53:54 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	counter = 0;
 	org = ((char *)s);
+	if (s == NULL)
+		return (NULL);
 	len_s = ft_strlen((char *)s);
 	if (start > len_s || len == 0)
 		return (ft_strdup(""));
