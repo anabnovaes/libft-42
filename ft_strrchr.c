ae43e24860803f6a33720fed25a616e804828973
Ana Novaes <ana-paula1503@hotmail.com>
2021-02-28 12:15:02 -0300

    corrigindo erros da norminette

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,17 +6,17 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 19:22:27 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/28 12:12:10 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/02/28 12:14:33 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strrchr(const char *str, int c)
+char	*ft_strrchr(const char *str, int c)
 {
 	char	*value;
 	int		counter;
-	
+
 	counter = ft_strlen((char *)str);
 	value = (char *)str;
 	while (counter >= 0)
