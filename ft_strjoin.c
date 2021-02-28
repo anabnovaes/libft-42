b367b55e50b59e1693961399958a4c923bfc10ef
Ana Novaes <ana-paula1503@hotmail.com>
2021-02-28 11:17:50 -0300

    adicionando mais validacoes

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 15:03:08 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/28 10:58:21 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/02/28 11:17:22 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	unsigned int	size_s2;
 	char			*join;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 	size_s1 = ft_strlen((char *)s1);
 	size_s2 = ft_strlen((char *)s2);
-	if (!(join = malloc(size_s1 + size_s2 + 1)) || (s1 == NULL || s2 == NULL))
+	if (!(join = malloc(size_s1 + size_s2 + 1)))
 		return (NULL);
 	ft_strlcpy(join, s1, size_s1 + 1);
 	ft_strlcat(join, s2, size_s2 + size_s1 + 1);
