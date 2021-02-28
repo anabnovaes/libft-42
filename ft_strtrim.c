650c8c7acc09dee3999d3b3e98c21e5676be758e
Ana Novaes <ana-paula1503@hotmail.com>
2021-02-28 11:05:01 -0300

    corrigindo ft strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 17:13:07 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/24 21:18:12 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/02/28 11:04:36 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	counter;
 
 	counter = 0;
-	size = ft_strlen((char *)s1);
 	if (s1 == NULL || set == NULL)
 		return (NULL);
+	size = ft_strlen((char *)s1);
 	while (ft_strchr(set, s1[counter]) && s1[counter] != '\0')
 	{
 		counter++;
