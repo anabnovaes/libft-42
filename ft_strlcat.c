9b6bc7411fda9363ca0b2fb23697cd6a0a25d880
Ana Novaes <ana-paula1503@hotmail.com>
2021-02-28 11:46:59 -0300

    add validacao strcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 19:22:09 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/24 19:55:07 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/02/28 11:46:33 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t size_dest;
 	size_t counter;
+	size_t size_org;
 
 	counter = 0;
 	size_dest = ft_strlen(dst);
-	if (size_dest > dstsize)
-		return (dstsize + ft_strlen((char *)src));
+	size_org = ft_strlen((char *)src);
+	if (size_dest >= dstsize)
+		return (dstsize + size_org);
 	while (counter + size_dest < dstsize - 1 && src[counter])
 	{
 		dst[size_dest + counter] = src[counter];
 		counter++;
 	}
 	dst[size_dest + counter] = '\0';
-	return (size_dest + ft_strlen((char *)src));
+	return (size_dest + size_org);
 }
--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 19:22:17 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/28 11:24:12 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/02/28 11:40:38 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ size_t	ft_strlen(char *s)
 	int size;
 
 	size = 0;
-	if (!s)
-		return (size);
 	while (s[size])
 	{
 		size++;
