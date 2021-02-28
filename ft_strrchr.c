2fe05245e7e8a6fda55782559330ca23f8b6285b
Ana Novaes <ana-paula1503@hotmail.com>
2021-02-28 11:56:47 -0300

    add valicacao strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 19:22:27 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/28 11:54:34 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/02/28 11:56:26 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ char	*ft_strrchr(const char *s, int c)
 	size_t length;
 
 	length = ft_strlen((char *)s);
-	while (length >= 0)
+	while (length > 0)
 	{
 		if (s[length] == c)
 			return ((char *)s + length);
 		length--;
 	}
+	if (s[length] == c)
+		return ((char *)s + length);
 	return (NULL);
 }
