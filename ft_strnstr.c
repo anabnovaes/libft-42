af98b84186acc81b7e5f68a8b71e68ddd86f7140
Ana Novaes <ana-paula1503@hotmail.com>
2021-05-01 18:01:40 -0300

    Adicionando norma

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 19:22:24 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/02/24 19:57:17 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/05/01 17:57:07 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t c1;
-	size_t c2;
+	size_t	c1;
+	size_t	c2;
 
 	c1 = 0;
 	if (*needle == '\0')
@@ -23,8 +23,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	while (c1 < len && haystack[c1] != '\0')
 	{
 		c2 = 0;
-		while ((haystack[c1 + c2] == needle[c2] && c1 + c2 < len) &&
-		needle[c2])
+		while ((haystack[c1 + c2] == needle[c2] && c1 + c2 < len)
+			&& needle[c2])
 			c2++;
 		if (needle[c2] == '\0')
 			return ((char *)haystack + c1);
