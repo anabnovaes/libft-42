87af7a7a818bb697fca2ba5e2778ef27454ea0c8
Ana Paula Novaes <ana-paula1503@hotmail.com>
2021-06-06 11:26:43 -0300

    retomando putnbr padrao

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,30 +6,62 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 17:52:09 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/06/06 11:17:34 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/06/06 11:26:23 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	check_size(long int value)
+{
+	int			size;
+	long int	compare_value;
+
+	size = 1;
+	compare_value = value;
+	while ((compare_value / 10) > 0)
+	{
+		compare_value /= 10;
+		size++;
+	}
+	return (size);
+}
+
+static void	cnvr_vlue(long int value, int size, int fd)
+{
+	int			last_value;
+	char		string_print[size + 1];
+	int			counter;
+	int			i;
+
+	i = 0;
+	counter = 1;
+	while (counter <= size)
+	{
+		last_value = value % 10;
+		value /= 10;
+		string_print[size - counter] = last_value + '0';
+		counter++;
+	}
+	string_print[size] = '\0';
+	while (string_print[i] != '\0')
+	{
+		write(fd, &string_print[i], 1);
+		i++;
+	}
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	int	counter;
-	int	print;
+	long int	print_value;
+	int			size;
 
-	counter = n;
-	if (counter < 0)
+	print_value = n;
+	if (n < 0)
 	{
-		ft_putchar_fd('-', fd);
-		counter *= -1;
+		write(fd, "-", 1);
+		print_value *= -1;
 	}
-	if (counter < 10)
-	{
-		print = counter + 48;
-		ft_putchar_fd(print, fd);
-		return ;
-	}
-	else
-		ft_putnbr_fd(counter / 10, fd);
-	ft_putnbr_fd(counter % 10, fd);
+	size = check_size(print_value);
+	cnvr_vlue(print_value, size, fd);
 }
